403d1c94ea5740b87dc58a3a6e6346df0107319f
Phan Shi Yu <phanshiyu@gmail.com>
2022-08-12 22:57:21 +0800

    chore: remove jest config

--- jest.config.ts
+++
@@ -1,8 +0,0 @@
-import type {Config} from '@jest/types';
-// Sync object
-const config: Config.InitialOptions = {
-  verbose: true,
-  preset: 'ts-jest',
-  testEnvironment: 'node'
-};
-export default config;
